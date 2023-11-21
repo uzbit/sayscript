911107f42ff31028fd41cd39340ef60fdbf3f7fc
Ted McCormack <ted@wootrex.biz>
2023-11-20 23:19:49 -0700

    adding GPT version

--- getoutofmyhouse.sh
+++ getoutofmyhouse.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+# Updated by asking GPT4 how to make the current version.
+# Get a list of available voices
+voices=($(say -v '?' | awk '{print $1}'))
 
-cat /usr/share/dict/words |grep -i $1|gshuf | while read p ; do   echo $p; say -v Ralph $p ; done
+# How many words to speak
+word_count=100
+
+# Iterate and speak random words
+for i in $(seq 1 $word_count); do
+    # Select a random voice
+    voice=${voices[$RANDOM % ${#voices[@]}]}
+
+    # Select a random word
+    word=$(shuf -n 1 /usr/share/dict/words)
+
+    echo "Speaking word: $word with voice: $voice"
+    say -v "$voice" "$word"
+done
